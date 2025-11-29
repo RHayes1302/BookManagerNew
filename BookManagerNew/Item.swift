ceb4c95b1f03f07636273f531884084ba43d4339
RHayes1302 <ramone.hayes@yahoo.com>
2025-11-28 22:24:51 -0800

    Initial Commit

---
+++ BookManagerNew/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  BookManagerNew
+//
+//  Created by Ramone Hayes on 11/28/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ BookManagerNewTests/BookManagerNewTests.swift
@@ -0,0 +1,17 @@
+//
+//  BookManagerNewTests.swift
+//  BookManagerNewTests
+//
+//  Created by Ramone Hayes on 11/28/25.
+//
+
+import Testing
+@testable import BookManagerNew
+
+struct BookManagerNewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
